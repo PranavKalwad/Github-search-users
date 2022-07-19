f3597dc68f17959a67cafc199f63c81af6dbf178
unknown <pranavkalwad@gmail.com>
2022-07-19 22:39:43 +0530

    finished search component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,10 +6,10 @@ const Dashboard = () => {
   return (
     <main>
       {/* <Navbar></Navbar> */}
-      {/* <Search /> */}
+      <Search />
       <Info />
       <User />
-      {/* <Repos /> */}
+      <Repos />
     </main>
   )
 }
